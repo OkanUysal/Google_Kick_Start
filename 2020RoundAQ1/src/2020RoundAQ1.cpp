3ee8a7ebf7fea623af10adb5ea0024fac467aa37
OkanUYSAL <uysal.okan.2010@gmail.com>
2020-10-23 16:24:21 +0300

    2020RoundAQ1

---
+++ 2020RoundAQ1/src/2020RoundAQ1.cpp
@@ -0,0 +1,54 @@
+//============================================================================
+// Name        : 2020RoundAQ1.cpp
+// Author      : Okan UYSAL
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve() {
+	int n, x, amount = 0, count = 0;
+	cin >> n >> x;
+	vector<int> a(n);
+
+	for(int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a.begin(), a.end());
+
+//	for(int i = 0; i < n; i++) {
+//		cout << ' ' << a[i];
+//	}
+
+	for(int i = 0; i < n; i++) {
+		if(amount + a[i] > x) {
+			break;
+		}
+		amount += a[i];
+		count++;
+
+	}
+
+	cout << count << '\n';
+
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	for( int i = 1; i <= t; i++) {
+		cout << "Case #" << i << ": ";
+		solve();
+	}
+
+	return 0;
+}
